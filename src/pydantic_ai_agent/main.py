0634806a188349f3de51bf703029b79ddba3a3a8
Pawel Trojanski <37036776+anybody84@users.noreply.github.com>
2025-07-12 11:59:23 +0200

    Add logfire observability (#5)

--- src/pydantic_ai_agent/main.py
+++ src/pydantic_ai_agent/main.py
@@ -1,11 +1,15 @@
 import asyncio
 
+import logfire
 from pydantic_ai import Agent
 from pydantic_ai.settings import ModelSettings
 
 from pydantic_ai_agent.llm.models import open_ai_model
 from pydantic_ai_agent.mcp_servers.notion_mcp_server import NotionMCPServerStdio
 from pydantic_ai_agent.settings.settings import settings
+
+logfire.configure(send_to_logfire="if-token-present", token=settings.logfire_token, console=False)
+logfire.instrument_pydantic_ai()
 
 system_prompt = """
 You are a helpful AI kitchen assistant specialized in finding recipes.
@@ -51,20 +55,23 @@ def instructions():
 
 
 async def main():
+    logfire.info("Starting application")
     logo()
     instructions()
     message_history = []
     user_input = get_user_input("What would you like to cook today?")
     async with recipe_agent.run_mcp_servers():
         while True:
-            result = await recipe_agent.run(
-                f"Provide me with a recipe for {user_input}", message_history=message_history
-            )
-            message_history.extend(result.new_messages())
-            user_input = get_user_input(result.output)
-            if should_end_conversation(user_input):
-                print("Ending conversation. Goodbye!")
-                break
+            prompt = f"Provide me with a recipe for {user_input}"
+            with logfire.span(f"User: {prompt}"):
+                result = await recipe_agent.run(prompt, message_history=message_history)
+                message_history.extend(result.new_messages())
+                user_input = get_user_input(result.output)
+                if should_end_conversation(user_input):
+                    print("Ending conversation. Goodbye!")
+                    break
+
+        logfire.info("Exiting application")
 
 
 if __name__ == "__main__":
